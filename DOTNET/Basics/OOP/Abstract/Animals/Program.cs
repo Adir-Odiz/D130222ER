d6b93702e0411d7a15d8f1c508b417a107431416
Adir-Odiz <adir2293@gmail.com>
2022-11-06 18:45:07 +0200

    a

---
+++ DOTNET/Basics/OOP/Abstract/Animals/Program.cs
@@ -0,0 +1,38 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Animal[] animals = new Animal[]
+        {
+            new Dog(),
+            new Snake(),
+            new Dog()
+        };
+
+        foreach (var animal in animals)
+        {
+            animal.AnimalSound();
+        }
+    }
+}
+
+
+
+abstract class Animal {
+    public abstract void AnimalSound();
+}
+
+class Snake : Animal 
+{
+    public override void AnimalSound()
+    {
+        System.Console.WriteLine("hissss");
+    }
+}
+class Dog : Animal 
+{
+    public override void AnimalSound()
+    {
+        System.Console.WriteLine("meow");
+    }
+}
